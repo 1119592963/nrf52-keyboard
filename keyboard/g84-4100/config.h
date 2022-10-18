aa9230d3e335b25495aaff8710a6f2cc0bef33e2
jelly <69285830+1119592963@users.noreply.github.com>
2022-10-18 18:43:27 +0800

    Update config.h

--- keyboard/g84-4100/config.h
+++ keyboard/g84-4100/config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CONF_PRODUCT_ID 0x4100 /* 配置项目内显示的ProductID */
 #define DEVICE_VER 0x0001 /* 硬件版本 */
 #define MANUFACTURER "Lotlab" /* 硬件制造商，用于蓝牙显示 */
-#define PRODUCT "BLE-4100" /* 硬件名称，用于USB和蓝牙显示 */
+#define PRODUCT "Z-BLE" /* 硬件名称，用于USB和蓝牙显示 */
 #define MACADDR_SEPRATOR '_' /* 蓝牙名称后地址的分隔符。若不设置则不显示蓝牙名称后面的地址 */
 
 /* USB HID report parameter */
@@ -48,17 +48,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KEYBOARD_SCAN_INTERVAL 1 // 键盘最小时间单位TICK (ms)
 #define KEYBOARD_FAST_SCAN_INTERVAL 10 // 通常模式下，多久扫描一次键盘 (ms)
 #define KEYBOARD_SLOW_SCAN_INTERVAL 100 // 慢速模式下，多久扫描一次键盘 (ms)
-#define LED_AUTOOFF_TIME 5 /* LED自动熄灭时长(s)，设为0则不自动熄灭 */
+//#define LED_AUTOOFF_TIME 5 /* LED自动熄灭时长(s)，设为0则不自动熄灭 */
+
+// LED自动熄灭时长(5000ms)，设为0则不自动熄灭
+#define LED_AUTOOFF_TIME 500
 
 // 键盘额外功能
-#define DYNAMIC_TX_POWER /* 启用自动发射功率调整 */
+//#define DYNAMIC_TX_POWER /* 启用自动发射功率调整 */
 #define PASSKEY_REQUIRED /* 需要输入配对码 */
 #define ENABLE_WATCHDOG /* 启用看门狗 */
+#define HIGH_TX_POWER /* 更改发射功率到+4dBm */
 #define KEYMAP_STORAGE /* 启用keymap存储 */
+#define MULTI_DEVICE_SWITCH  /*启用多设备切换 */
 // #define MACRO_BLOCKING_MODE /* 在宏播放时禁用其他按键输入 */
-#define MACRO_STORAGE /* 启用宏存储功能 */
-#define CONFIG_STORAGE /* 启用配置存储功能 */
-#define BUTTONLESS_DFU /* 启用免按钮DFU */
+//#define MACRO_STORAGE /* 启用宏存储功能 */
+//#define CONFIG_STORAGE /* 启用配置存储功能 */
+//#define BUTTONLESS_DFU /* 启用免按钮DFU */
 
 // #define DEBUG_SKIP_PWRON_CHECK /*  直接开机而跳过开机条件检测，用于调试 */
 
@@ -81,41 +86,49 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // LED 配置
 // #define LED_NUM 25
-#define LED_CAPS 24
+#define LED_CAPS 29
 // #define LED_SCLK 23
-// #define LED_POSITIVE // LED下拉驱动
-#define LED_DFU_INIT 25
-#define LED_DFU_START 24
-#define LED_DFU_FINISH 23
+#define LED_POSITIVE // LED下拉驱动
+//#define LED_DFU_INIT 25
+//#define LED_DFU_START 24
+//#define LED_DFU_FINISH 23
 // #define LED_DFU_POSITIVE
+
+// 启用 LED 状态灯
+#define LED_BLE 30  // 19
+
+// 独立硬件按钮
+#define POWER_BUTTON 31 // 3
 
 // USB UART 传输配置
 #define HAS_USB // 启用与CH554的通信支持
-#define UART_RXD 27 // UART_RX口IO
-#define UART_TXD 26 // UART_TX口IO
+#define UART_RXD 17 // UART_RX口IO
+#define UART_TXD 18 // UART_TX口IO
 // #define UART_DET 19 // UART 检测引脚，若此脚被拉低，则说明USB正在工作。若不配置则使用RX口作为检测引脚
 #define UART_BAUDRATE NRF_UART_BAUDRATE_115200 // 通信波特率，请不要修改
+#define CH55X_FREQ_SYS 24000000
 
 // 电量检测配置
-#define BATTERY_ADC_PIN NRF_SAADC_INPUT_AIN0 // 电量检测引脚
+#define BATTERY_ADC_PIN NRF_SAADC_INPUT_AIN0 // 电量检测引脚 PIN2
 
 // 充电检测配置
 #define PIN_CHARGING !UCC1 // CH554的充电检测。当UCC1拉低时表示正在充电
 #define PIN_STANDBY !UCC2 // CH554的充电检测。当UCC2拉低时表示充电完成。若不配置则只使用PIN_CHARGING作为是否充电的检测标志
 
 // 按键阵列配置
-#define MATRIX_ROWS 8 /* 硬件阵列行数 */
-#define MATRIX_COLS 14 /* 硬件阵列列数 */
-static const uint8_t row_pin_array[MATRIX_ROWS] = { 9, 8, 7, 6, 5, 4, 3, 11 };
-static const uint8_t column_pin_array[MATRIX_COLS] = { 16, 17, 18, 19, 20, 21, 22, 28, 29, 30, 15, 14, 13, 12 };
+#define MATRIX_ROWS 6 /* 硬件阵列行数 */
+#define MATRIX_COLS 17 /* 硬件阵列列数 */
+static const uint8_t row_pin_array[MATRIX_ROWS] = { 3, 4, 5, 6, 24, 25 };
+static const uint8_t column_pin_array[MATRIX_COLS] = { 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 26, 27, 19, 20, 21, 22, 23 };
 #define ROW_IN // 键盘阵列的二极管方向是从COL->ROW
 
 /* define if matrix has ghost */
-#define MATRIX_HAS_GHOST /* 按键阵列是否出现Ghost Key，若没有加二极管则需要启用这个项目 */
+//#define MATRIX_HAS_GHOST /* 按键阵列是否出现Ghost Key，若没有加二极管则需要启用这个项目 */
 
 #define DEBOUNCE 5 /* 硬件消抖次数，设置为0则不消抖 */
 #define MATRIX_SCAN_DELAY_CYCLE 36 /* 按键扫描等待IO稳定的延时时长 */
 
+/*
 #ifdef NRF52_APP
 // 定义两个LED灯
 static const struct ledmap_led ledmap_leds[] = { { .dir = 0, .pin = 25 }, { .dir = 0, .pin = 23 } };
@@ -191,3 +204,4 @@ static const struct ledmap_event ledmap_events[] = {
     },
 };
 #endif
+*/
